bf621edce5bf6329fd54102e16e7f2ecd814597b
Brahim SLIMANI <36894964+brahim-slimani@users.noreply.github.com>
2023-01-20 20:11:53 +0100

    Update footer.jsx

--- src/component/footer.jsx
+++ src/component/footer.jsx
@@ -5,7 +5,7 @@ export const Footer = () => {
     <footer className="page-footer font-small">
       {/* Copyright  */}
       <div className="footer-copyright text-center py-3 footer-content">
-        © Copyright {new Date().getFullYear()}
+        © Copyright {2021}
       </div>
     </footer>
   );
